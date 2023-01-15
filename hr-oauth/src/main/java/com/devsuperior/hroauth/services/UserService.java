820ece73a178ce6cd0cae2adbbd9df0d21ee8c0f
celsonrd <celsonrd@gmail.com>
2023-01-15 12:20:26 -0300

    Login JWT

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -2,13 +2,16 @@ package com.devsuperior.hroauth.services;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(User.class);
 
@@ -25,4 +28,20 @@ public class UserService {
 		logger.info("Email found" + email);
 		return user;
 	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("Email not found" + username);
+			throw new UsernameNotFoundException("User not found");
+		}
+		logger.info("Email found" + username);
+		return user;
+	}
+	
+	
+	
+	
 }
